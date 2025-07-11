5d78d5f6199b7c0a93aeef8b0b0975f8482af9a5
Ankit Kumar Dam <aparnadam1981@gmail.com>
2025-07-12 03:28:26 +0530

    m

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -16,20 +16,22 @@ function Input({
         </label>
         <input
           value={amount}
-          onChange={(e) =>
-            onAmountChange && onAmountChange(Number(e.target.value))
-          }
+          onChange={(e) => {
+            const val = Number(e.target.value);
+            if (val >= 0) {
+              onAmountChange && onAmountChange(val);
+            }
+          }}
           type="number"
           className="outline-none w-full bg-transparent py-1.5 text-black"
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select size={2}
+        <select
+          size={2}
           value={selectCurrency}
-          onChange={(e) =>
-            onCurrencyChange && onCurrencyChange(e.target.value)
-          }
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none text-black"
         >
           {currencyOption.map((currency) => (
